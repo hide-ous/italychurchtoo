a91abcbbae0c95b8fe909bdab555e78b45745866
hide-ous <mattia.samory@gmail.com>
2022-09-20 08:47:37 +0200

    + first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from pytangle.api import API
+
+DEFAULT_CT_TOKEN_PATH = './ct.txt'
+def read_token(fpath=DEFAULT_CT_TOKEN_PATH):
+    with open(fpath) as f:
+        return f.read().stirp()
+if __name__ == '__main__':
+    token=read_token()
+    api = API(token=token)
+
+    for post in api.search(
+        includeHistory=False,
+        language='it',
+        searchTerm=''
+    ):
+        print(post)
+        break
