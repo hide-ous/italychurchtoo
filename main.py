c9512396d20ef8a1f425546b949c91b46987b7e5
hide-ous <mattia.samory@gmail.com>
2022-09-20 08:47:57 +0200

    * typo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pytangle.api import API
 DEFAULT_CT_TOKEN_PATH = './ct.txt'
 def read_token(fpath=DEFAULT_CT_TOKEN_PATH):
     with open(fpath) as f:
-        return f.read().stirp()
+        return f.read().strip()
 if __name__ == '__main__':
     token=read_token()
     api = API(token=token)
